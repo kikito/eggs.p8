df093a86d2990787a26bff88b86e716ea89f5761
kikito <kikito@gmail.com>
2025-10-05 16:23:22 +0200

    fix indentation in test_eggs.lua

--- test_eggs.lua
+++ test_eggs.lua
@@ -11,11 +11,11 @@ local tick, grow, scan, wander, flee, move, hunt, lay, vanish, census, draw
 local stats, cpu, pop, maxpop
 local thebeast
 local phases = { -- how many seconds:
-	5,  -- egg,
-	7,  -- child,
-	6,  -- teen,
-	12, -- adult,
-	1,  -- corpse
+  5,  -- egg,
+  7,  -- child,
+  6,  -- teen,
+  12, -- adult,
+  1,  -- corpse
 }
 
 local phaseages = {}
@@ -28,7 +28,7 @@ for i = 1, #phases do
   if i == 5 then
     corpseage = maxage + 1
   end
-	maxage += phases[i] * 30
+  maxage += phases[i] * 30
 end
 
 local function kill(e)
@@ -40,47 +40,47 @@ end
 
 function _init()
   world = eggs()
-	stats = true
+  stats = true
   cpu = 0
   pop = 0
   maxpop = 0
 
-	chick = function(g, x, y)
+  chick = function(g, x, y)
     if cpu>=.95 then return end
-		g = g or rnd({ "m", "f" })
-		local age = age or 1+flr(rnd(120))
-		local p = phaseages[age]
-		local s = p + (g == "m" and 4 or 0)
-		return world.ent("pos,sprite,age,phase,genre,alive", {
-			g = g,
-			x = x or rnd(120),
-			y = y or rnd(120),
-			vx = 0,
-			vy = 0,
-			age = age,
-			p = p,
-			s = s,
-		})
-	end
+    g = g or rnd({ "m", "f" })
+    local age = age or 1+flr(rnd(120))
+    local p = phaseages[age]
+    local s = p + (g == "m" and 4 or 0)
+    return world.ent("pos,sprite,age,phase,genre,alive", {
+      g = g,
+      x = x or rnd(120),
+      y = y or rnd(120),
+      vx = 0,
+      vy = 0,
+      age = age,
+      p = p,
+      s = s,
+    })
+  end
 
-	beast = function()
-		return world.ent("pos,vel,sprite", {
-			x = rnd(120),
-			y = rnd(120),
-			vx = 0.5 + rnd(0.5),
-			vy = 0.5 + rnd(0.5),
-			s = 255,
-		})
-	end
+  beast = function()
+    return world.ent("pos,vel,sprite", {
+      x = rnd(120),
+      y = rnd(120),
+      vx = 0.5 + rnd(0.5),
+      vy = 0.5 + rnd(0.5),
+      s = 255,
+    })
+  end
 
   tick = world.sys("age", function(e)
-		e.age += 1
+    e.age += 1
   end)
 
   -- change phase according to age, add/remove tags in some phase changes
-	grow = world.sys("age,alive,phase,genre,sprite", function(e)
+  grow = world.sys("age,alive,phase,genre,sprite", function(e)
     local np = assert(phaseages[e.age], e.age)
-		if np == e.p then return end
+    if np == e.p then return end
     e.p = np
     if np==5 then
       kill(e)
@@ -92,7 +92,7 @@ function _init()
         world.tag(e,"laying")
       end
     end
-	end)
+  end)
 
   -- any chickens that are alive and close to the beast are marked as prey
   scan = world.sys("alive,pos", function(e)
@@ -118,10 +118,10 @@ function _init()
     0,   -- corpse
   }
   -- move around randomly
-	wander = world.sys("vel,genre,phase", function(e)
+  wander = world.sys("vel,genre,phase", function(e)
     local v = adhd[e.p] + (e.g == "m" and 1 or 0) -- males are more hyperactive
-		e.vx = e.vx+rnd(2*v)-v
-		e.vy = e.vy+rnd(2*v)-v
+    e.vx = e.vx+rnd(2*v)-v
+    e.vy = e.vy+rnd(2*v)-v
   end)
 
   -- flee from the beast when it is preyby
@@ -134,17 +134,17 @@ function _init()
   end)
 
   -- update position according to velocity, bouncing on walls and limiting max speed
-	move = world.sys("pos,vel", function(e)
-		local x, vx = e.x, e.vx
-		vx = x < 0 and abs(vx) or x > 120 and -abs(vx) or vx
-		e.vx = mid(-2,vx,2)
-		e.x = x + e.vx
+  move = world.sys("pos,vel", function(e)
+    local x, vx = e.x, e.vx
+    vx = x < 0 and abs(vx) or x > 120 and -abs(vx) or vx
+    e.vx = mid(-2,vx,2)
+    e.x = x + e.vx
 
-		local y, vy = e.y, e.vy
-		vy = y < 0 and abs(vy) or y > 120 and -abs(vy) or vy
+    local y, vy = e.y, e.vy
+    vy = y < 0 and abs(vy) or y > 120 and -abs(vy) or vy
     e.vy = mid(-2,vy,2)
-		e.y = y + e.vy
-	end)
+    e.y = y + e.vy
+  end)
 
   -- kill prey by colliding with them
   hunt = world.sys("prey,pos", function(e)
@@ -156,34 +156,34 @@ function _init()
     end
   end)
 
-	lay = world.sys("laying,pos", function(e)
-		if rnd() > .99 then
-			chick(nil, e.x, e.y)
-		end
-	end)
+  lay = world.sys("laying,pos", function(e)
+    if rnd() > .99 then
+      chick(nil, e.x, e.y)
+    end
+  end)
 
-	vanish = world.sys("dead", function(e)
-		if e.age >= maxage then
+  vanish = world.sys("dead", function(e)
+    if e.age >= maxage then
       world.del(e) -- remove entity from the world
-		end
-	end)
+    end
+  end)
 
   -- a system that has no tags ("") will run for every entity in eggs
   local census_sys = world.sys("", function()
-		pop += 1
-	end)
+    pop += 1
+  end)
   -- Wrap systems into container functions in order to do "pre" and "post" actions
-	census = function()
+  census = function()
     pop = 0
     census_sys()
-	  if pop > 100 and not thebeast then
-		  thebeast = beast()
-	  end
+    if pop > 100 and not thebeast then
+      thebeast = beast()
+    end
     maxpop = max(maxpop, pop)
   end
 
   local draw_e = function(e)
-		spr(e.s, e.x, e.y, 1, 1, e.vx and e.vx < 0)
+    spr(e.s, e.x, e.y, 1, 1, e.vx and e.vx < 0)
     --local msk=world.msk(e)
     --if msk.prey then
     --  circ(e.x+4,e.y+4,6,14)
@@ -193,11 +193,11 @@ function _init()
   local draw_dead = world.sys("pos,sprite,dead", draw_e)
   local draw_alive = world.sys("pos,sprite,alive", draw_e)
 
-	draw = function()
+  draw = function()
     draw_dead()
     draw_alive()
     if thebeast then draw_e(thebeast) end
-	end
+  end
 
   for x=0,15 do
     for y=0,15 do
@@ -213,29 +213,29 @@ end
 
 function _update()
   tick()
-	grow()
-	wander()
+  grow()
+  wander()
   if thebeast then
     scan()
     flee()
   end
-	move()
+  move()
   if thebeast then hunt() end
-	lay()
+  lay()
   vanish()
-	census()
+  census()
 
-	if btnp(4) then
-		stats = not stats
-	end
+  if btnp(4) then
+    stats = not stats
+  end
 
-	if btnp(5) then
-		chick()
-	end
+  if btnp(5) then
+    chick()
+  end
 end
 
 function _draw()
-	cls(3)
+  cls(3)
 
   draw()
 
